848d825572f1d6afb2e673ebcc5491810670f741
LiuJunDa <3364863955@qq.com>
2026-04-13 15:29:34 +0800

    统一的项目管理

---
+++ backend/ckqa-back/src/main/java/org/ysu/ckqaback/CkqaBackApplication.java
@@ -0,0 +1,13 @@
+package org.ysu.ckqaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkqaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkqaBackApplication.class, args);
+    }
+
+}
---
+++ backend/ckqa-back/src/test/java/org/ysu/ckqaback/CkqaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ysu.ckqaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkqaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
